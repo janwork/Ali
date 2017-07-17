14c4dd52af86dd375d52f365360ee6931546c1cd
janwork <janwork@163.com>
2017-07-17 21:59:43 +0800

    sortPrice add js

---
+++ sortPrice.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Jan on 2017/7/17.
+ */
+
+$(document).ready(function () {
+  $("input.sortBarPrice").keyup(function () {
+             var num = $(this).val();
+             if (num.length == 0){
+                 $("div.productUnit").show();
+                 return;
+             }
+      num =  parseInt(num);
+      if(isNaN(num)){
+          num = 1;
+      }
+
+      if(isNaN(num) <= 0){
+          num = 0;
+      }
+
+      var begin = $("input.beginPrice").val();
+      var end = $("input.endPrice").val();
+      if(!isNaN(begin) && !isNaN(end)){
+          console.log(begin);
+          console.log(end);
+
+          $("div.productUnit").hide();
+          $("div.productUnit").each(function () {
+              var price = $(this).attr("price");
+              price = new Number(price);
+
+              if(price<=end && price >=begin){
+                  $(this).show();
+              }
+          });
+
+
+      }
+
+  });
+});
+
+
